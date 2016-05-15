8e65ee38cf33391a1845b9d57dd641f6f8883a23
Josh Pullen <JoshPullen27@gmail.com>
2016-05-15 16:14:23 -0400

    Update open-webpage.js

--- open-webpage.js
+++ open-webpage.js
@@ -30,7 +30,11 @@
             link.media = 'all';
             head.appendChild(link);
         }
-        var popup_html = '<div class=owext-darken id=owext-modal><div class=owext-inner><div class=owext-url><div class=owext-tophalf><h2>Open this Webpage?</h2><div>The project wants to open</div><a style=color:#21b4f0!important;font-weight:700>' + escaped_url + '</a></div><div class=owext-bottomhalf><script>function close_owext_modal(){var e=document.getElementById("owext-modal");e.parentNode.removeChild(e)}</script><a style=background:#BBBDC0 onclick=close_owext_modal()>Exit</a> <a style=background:#21b4f0 onclick=\'close_owext_modal(),window.open("' + escaped_url + '","_blank")\'>Open</a></div></div></div></div>';
+        close_owext_modal = function() {
+            var elem = document.getElementById("owext-modal");
+            elem.parentNode.removeChild(elem);
+        }
+        var popup_html = '<div class=owext-darken id=owext-modal><div class=owext-inner><div class=owext-url><div class=owext-tophalf><h2>Open this Webpage?</h2><div>The project wants to open</div><a style=color:#21b4f0!important;font-weight:700>' + escaped_url + '</a></div><div class=owext-bottomhalf><a style=background:#BBBDC0 onclick=close_owext_modal()>Exit</a> <a style=background:#21b4f0 onclick=\'close_owext_modal(),window.open("' + escaped_url + '","_blank")\'>Open</a></div></div></div></div>';
         document.body.innerHTML += popup_html;
         window.setTimeout(function() { // 10 second delay for testing only
             callback();
