35a97dd4b410ad700ec9a35fd4115efd70596dad
Josh Pullen <JoshPullen27@gmail.com>
2016-05-15 16:24:29 -0400

    Update open-webpage.js

--- open-webpage.js
+++ open-webpage.js
@@ -35,6 +35,7 @@
             elem.parentNode.removeChild(elem);
         }
         var popup_html = '<div class=owext-darken id=owext-modal><div class=owext-inner><div class=owext-url><div class=owext-tophalf><h2>Open this Webpage?</h2><div>The project wants to open</div><a style=color:#21b4f0!important;font-weight:700>' + escaped_url + '</a></div><div class=owext-bottomhalf><a style=background:#BBBDC0 onclick=close_owext_modal()>Exit</a> <a style=background:#21b4f0 onclick=\'close_owext_modal(),window.open("' + escaped_url + '","_blank")\'>Open</a></div></div></div></div>';
+        console.log(popup_html);
         
         var dummyElem = document.createElement('div');
         dummyElem.innerHTML = popup_html;
