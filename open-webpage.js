dcc14fd0dfaa41dd8c377780eeef70b8ae0b7ea9
Josh Pullen <JoshPullen27@gmail.com>
2016-05-15 19:11:44 -0400

    Update open-webpage.js

--- open-webpage.js
+++ open-webpage.js
@@ -36,7 +36,7 @@
         }
         console.log("v1");
         var popup_html = '<div class=owext-darken id=owext-modal><div class=owext-inner><div class=owext-url><div class=owext-tophalf><h2>Open this Webpage?</h2><div>The project wants to open</div><a style=color:#21b4f0!important;font-weight:700>' + escaped_url + '</a></div><div class=owext-bottomhalf><a style=background:#BBBDC0 onclick=close_owext_modal()>Exit</a> <a style=background:#21b4f0 onclick=\'close_owext_modal(),window.open("' + escaped_url + '","_blank")\'>Open</a></div></div></div></div>';
-        $(body).append(popup_html); // Is jquery safe to use?
+        $("body").append(popup_html); // Is jquery safe to use?
         
         window.setTimeout(function() { // 10 second delay for testing only
             callback();
